07b622937c6b4b7749dcc6bb261919cfa46a0870
kcaiueo <kcwebapply@gmail.com>
2019-09-11 08:08:09 +0900

    add register func

--- domain/service/register.go
+++ domain/service/register.go
@@ -1,9 +1,16 @@
 package service
 
 import (
+	"fmt"
+	"net/url"
+
 	"github.com/gin-gonic/gin"
 )
 
-func RegisterService(c *gin.Context) {
+func RegisterService(ctx *gin.Context) {
+	serviceURL := ctx.GetHeader("service-url")
+	if _, err := url.Parse(serviceURL); err != nil {
+		fmt.Println("error parsing!", serviceURL)
+	}
 
 }
